fc56e0e231bedc5d2f0be1b693e4ef2155b9e70c
bittmario <jamario@bitt.com>
2022-11-20 21:10:32 -0400

    Updated styles in the form.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -135,7 +135,10 @@ const Form = ({ updatePassword }: FormProps): JSX.Element => {
             <StrengthIndicator passwordStrength={passwordStrength} />
             <button form="strengthForm" className={styles.submitButton}>
                 Generate
-                <img src={arrowImage} alt="Right arrow" />
+                {/* <img src={arrowImage} alt="Right arrow" /> */}
+                <svg className={styles.copyIcon} width="12" height="12" xmlns="http://www.w3.org/2000/svg">
+                    <path d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z" />
+                </svg>
             </button>
         </div>
     );
